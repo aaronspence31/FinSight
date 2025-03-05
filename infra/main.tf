ae4b467fe390b22bdd2537e7ed77355ef80b7765
Aaron Spence <aaronspence31@gmail.com>
2025-03-05 11:57:13 -0500

    setup glue lambda handler and lambda infra

--- infra/main.tf
+++ infra/main.tf
@@ -80,3 +80,122 @@ resource "aws_s3_bucket_policy" "website" {
   # Wait for the public access block to be configured before applying the policy
   depends_on = [aws_s3_bucket_public_access_block.website]
 }
+
+# IAM role for Lambda to access AWS services
+resource "aws_iam_role" "lambda_role" {
+  name = "finsight-lambda-trigger-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "Lambda Execution Role"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+# IAM policy for Lambda to access S3 and start Glue job
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "finsight-lambda-trigger-policy"
+  description = "Policy for Lambda to access S3 and start Glue job"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect = "Allow"
+        Resource = [
+          aws_s3_bucket.raw_data.arn,
+          "${aws_s3_bucket.raw_data.arn}/*"
+        ]
+      },
+      {
+        Action = [
+          "glue:StartJobRun",
+          "glue:GetJobRun",
+          "glue:GetJobRuns",
+          "glue:BatchStopJobRun"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach IAM policy to IAM role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+# Lambda function to trigger Glue job
+resource "aws_lambda_function" "trigger_glue_job" {
+  function_name = "finsight-trigger-etl-job"
+  description   = "Triggers Glue ETL job when new data is uploaded to S3"
+  filename      = "${path.module}/../src/lambda/trigger_glue_job.zip"
+  handler       = "trigger_glue_job.lambda_handler"
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda_role.arn
+  environment {
+    variables = {
+      GLUE_JOB_NAME = "finsight-etl-job"
+      PROCESSED_BUCKET = aws_s3_bucket.processed_data.bucket
+    }
+  }
+  timeout       = 60
+  memory_size   = 128
+  tags = {
+    Name        = "ETL Trigger Function"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+# S3 notification to trigger Lambda when object is created
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.raw_data.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.trigger_glue_job.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = ""
+    filter_suffix       = ".csv"
+  }
+
+  # Ensure Lambda permissions are in place before notification
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
+
+# Permission for S3 to invoke Lambda
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.trigger_glue_job.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.raw_data.arn
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -22,3 +22,23 @@ output "website_url" {
   description = "Website URL"
   value       = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
 }
+
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.trigger_glue_job.arn
+}
+
+output "lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = aws_lambda_function.trigger_glue_job.function_name
+}
+
+output "lambda_role_arn" {
+  description = "The ARN of the IAM role used by the Lambda function"
+  value       = aws_iam_role.lambda_role.arn
+}
+
+output "s3_notification_configuration" {
+  description = "The S3 bucket notification configuration"
+  value       = aws_s3_bucket_notification.bucket_notification.id
+}
